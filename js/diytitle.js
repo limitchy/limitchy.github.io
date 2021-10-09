711f6e3241c28b28436007a846715349b724bc6a
limitchy <vcvchhjsk@gmail.com>
2021-10-09 14:23:02 +0800

    Site updated: 2021-10-09 14:23:02

--- js/diytitle.js
+++ js/diytitle.js
@@ -16,3 +16,12 @@ document.addEventListener('visibilitychange', function () {
         }, 2000);
     }
 });
+
+
+
+// 去掉banner图
+
+// var full_page = document.getElementsByClassName("full_page");
+// if (full_page.length != 0) {
+//   full_page[0].style.background = "transparent";
+// }
